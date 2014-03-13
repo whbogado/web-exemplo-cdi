b931abbd4d45d04d86dc9a1c0c4a73716dcbacdb
Wilson <Wilson@homeserver>
2014-03-13 08:14:52 -0300

    Inicial.

---
+++ src/main/java/cdi/named/LoginBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cdi.named;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Wilson
+ */
+@Named
+@SessionScoped
+public class LoginBean implements Serializable{
+    private String login;
+    private String senha;
+
+    public LoginBean() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public String loginAction() {
+        return null;
+    }
+}
